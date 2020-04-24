53e74f2d67235a1ee46346f8aa720e420f090b6c
遠藤洸剛 <hirotakeendo@endoukoutsuyoshinoMacBook-Pro.local>
2020-04-24 14:22:34 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '03504903037fc247416ba62d3614927556c2aa6ca1295dac5fbcdf6b0bd1f68cda784bd199943acd465b72b9795a9a8b36c422014988f7d4816051112250f3ad'
